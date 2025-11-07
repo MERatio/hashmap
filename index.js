d85b0016aaf2a55582d3ab5baba83c5832ff4e77
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-11-07 20:54:38 +0800

    Add set, print for HashMap's objects. and some logs for testing

---
+++ index.js
@@ -0,0 +1,27 @@
+import HashMap from './js/HashMap.js';
+
+const test = new HashMap();
+
+console.log('--- Add initial values ---');
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+test.print();
+
+console.log('--Trigger hashmap growth- --');
+test.set('moon', 'silver');
+test.print();
+
+console.log('---Update apple, and lion');
+test.set('apple', 'UPDATED!!!');
+test.set('lion', 'UPDATED!!!');
+test.print();
---
+++ js/HashMap.js
@@ -0,0 +1,97 @@
+import Node from './Node.js';
+
+class HashMap {
+  #loadFactor = 0.75;
+  #capacity = 16;
+  #buckets = new Array(16).fill(null);
+  #size = 0;
+
+  #hash(key, capacity) {
+    let hashCode = 0;
+
+    const PRIME_NUMBER = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (PRIME_NUMBER * hashCode + key.charCodeAt(i)) % capacity;
+    }
+
+    return hashCode;
+  }
+
+  #resize() {
+    const oldBuckets = this.#buckets;
+    this.#capacity *= 2;
+    this.#buckets = new Array(this.#capacity).fill(null);
+    this.#size = 0;
+
+    for (const head of oldBuckets) {
+      let cur = head;
+      while (cur !== null) {
+        this.set(cur.key, cur.value);
+        cur = cur.nextNode;
+      }
+    }
+  }
+
+  set(key, value) {
+    if (typeof key !== 'string') {
+      throw new TypeError('HashMap only supports string keys.');
+    }
+
+    // Check and resize at the start rather than at the end, to avoid re-inserting the newly inserted node.
+    if (this.#size + 1 > this.#capacity * this.#loadFactor) {
+      this.#resize();
+    }
+
+    const hashCode = this.#hash(key, this.#capacity);
+    const newNode = new Node(key, value);
+    const head = this.#buckets[hashCode];
+
+    // If the bucket is empty, add a node, and make it the head of the linked list.
+    if (head === null) {
+      this.#buckets[hashCode] = newNode;
+      this.#size++;
+    } else {
+      // If there's at least 1 node in the bucket, traverse the linked list.
+      let cur = head;
+      while (cur !== null) {
+        // If key matches a node's key, update its node's value.
+        if (cur.key === key) {
+          cur.value = value;
+          return;
+        }
+        // If key doesn't match a node's key. Append the new node at the end of the linked list.
+        if (cur.nextNode === null) {
+          cur.nextNode = newNode;
+          this.#size++;
+          return;
+        }
+        cur = cur.nextNode;
+      }
+    }
+  }
+
+  print() {
+    console.log('--- HashMap Debug ---');
+    console.log(`Size: ${this.#size}, Capacity: ${this.#capacity}`);
+    console.log();
+    for (let i = 0; i < this.#capacity; i++) {
+      let cur = this.#buckets[i];
+      const bucket = [];
+      while (cur !== null) {
+        bucket.push(`(${cur.key}: ${cur.value})`);
+        cur = cur.nextNode;
+      }
+
+      // Show the chain ending with null
+      if (bucket.length > 0) {
+        console.log(`[${i}] ${bucket.join(' -> ')} -> null`);
+      } else {
+        console.log(`[${i}] null`);
+      }
+    }
+    console.log();
+    console.log('--- HashMap Debug End ---');
+  }
+}
+
+export default HashMap;
